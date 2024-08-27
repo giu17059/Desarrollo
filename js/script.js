a6007a02078e9290891c293e264009e9b1cf75c8
Giuli <giulianotomas@hotmail.com.ar>
2024-08-27 20:19:10 -0300

    tercero

--- js/script.js
+++ js/script.js
@@ -4,17 +4,10 @@ const checkbox = document.getElementById('theme');
             if (checkbox.checked) {
                 document.body.style.backgroundColor = 'black';
                 document.getElementById('navegacion').style.backgroundColor = 'grey';
-<<<<<<< HEAD
                 document.getElementById('footer').style.color = 'white';
             } else {
                 document.body.style.backgroundColor = '';
                 document.getElementById('navegacion').style.backgroundColor = '';
                 document.getElementById('footer').style.color = '';
-=======
-                document.getElementById('navegacion').style.backgroundColor = 'white';
-            } else {
-                document.body.style.backgroundColor = '';
-                document.getElementById('navegacion').style.backgroundColor = '';
->>>>>>> a9624c13ff479e023fb4c6d299f046e36652fea4
             }
         });
