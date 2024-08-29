3c2621038b0cb10f9bead11c8708c7cad61be883
Giuli <giulianotomas@hotmail.com.ar>
2024-08-29 15:00:20 -0300

    boostrap

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const checkbox = document.getElementById('theme');
+const checkbox = document.getElementById('toggle');
 
         checkbox.addEventListener('change', function() {
             if (checkbox.checked) {
@@ -6,8 +6,8 @@ const checkbox = document.getElementById('theme');
                 document.getElementById('navegacion').style.backgroundColor = 'grey';
                 document.getElementById('footer').style.color = 'white';
             } else {
-                document.body.style.backgroundColor = '';
-                document.getElementById('navegacion').style.backgroundColor = '';
+                document.body.style.backgroundColor = ' #86f5f8';
+                document.getElementById('navegacion').style.backgroundColor = '#4fd5f0';
                 document.getElementById('footer').style.color = '';
             }
         });
