ec37ea407c7e83bc75b0ab2a4427c33e7ccaa725
Giuli <giulianotomas@hotmail.com.ar>
2024-08-30 13:19:35 -0300

    fin

--- js/script.js
+++ js/script.js
@@ -2,28 +2,26 @@ const checkbox = document.getElementById('toggle');
 
 checkbox.addEventListener('change', function () {
     if (checkbox.checked) {
-        document.body.style.backgroundColor = 'black';
-        document.getElementById('navegacion').style.backgroundColor = 'grey';
-        document.getElementById('footer').style.color = 'white';
-    } else {
-        document.body.style.backgroundColor = ' #86f5f8';
-        document.getElementById('navegacion').style.backgroundColor = '#09db68';
-        document.getElementById('footer').style.color = '';
-    }
-});
-checkbox.addEventListener('change', function () {
-    if (checkbox.checked) {
+        document.body.classList.add('body_oscuro');
+        document.querySelector('footer').classList.add('footer_oscuro');
+        document.getElementById('navegacion').classList.add('navegacion_oscuro');
+
         localStorage.setItem('dark-mode', 'enabled');
     } else {
+        document.body.classList.remove('body_oscuro');
+        document.querySelector('footer').classList.remove('footer_oscuro');
+        document.getElementById('navegacion').classList.remove('navegacion_oscuro');
+
         localStorage.setItem('dark-mode', 'disabled');
     }
 });
+
 window.addEventListener('load', function () {
     const darkMode = localStorage.getItem('dark-mode');
     if (darkMode === 'enabled') {
         checkbox.checked = true;
         document.body.classList.add('body_oscuro');
+        document.querySelector('footer').classList.add('footer_oscuro');
         document.getElementById('navegacion').classList.add('navegacion_oscuro');
-        document.getElementById('footer').classList.add('footer_oscuro');
     }
 });
